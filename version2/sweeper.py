09664993c37d4c2e18a3fd6da2e33e2967b840df
gauravmisrase <gauravmisra544@gmail.com>
2017-04-19 17:45:35 -0700

    Modified the sweeper logic

--- version2/sweeper.py
+++ version2/sweeper.py
@@ -2,10 +2,12 @@ from Order import posts
 import time
 
 while True:
-    time.sleep(5)
     update_paid = posts.update_many({'status': 'PAID'}, {'$set': {'status': 'PREPARING'}})
+    time.sleep(20)
     update_preparing = posts.update_many({'status': 'PREPARING'}, {'$set': {'status': 'SERVED'}})
+    time.sleep(20)
     update_served = posts.update_many({'status': 'SERVED'}, {'$set': {'status': 'COLLECTED'}})
+    time.sleep(20)
     print "number of orders preparing = " , update_paid.modified_count
     print "number of orders served = " , update_preparing.modified_count
     print "number of orders collected = " , update_served.modified_count
